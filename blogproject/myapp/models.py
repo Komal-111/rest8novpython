fd73399671669e99fe1b7b5f8cde6ee35ed9ce26
Komal-111 <118002852+Komal-111@users.noreply.github.com>
2023-05-03 15:38:58 +0530

    Delete blogproject directory

--- blogproject/myapp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from datetime import datetime
-
-# Create your models here.
-class blog(models.Model):
-    created=models.DateTimeField(default=datetime.now(),blank=True)
-    title=models.CharField(max_length=100)
-    content=models.CharField(max_length=100)
-    updated=models.DateTimeField(default=datetime.now(),blank=True)
